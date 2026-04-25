dfedfded7478142522695aba762359a737058c15
James Daniel Cadavona <63523469+Jameboyyy@users.noreply.github.com>
2026-04-24 17:53:15 -0700

    Set Up React(Vite) environment. File structured. Homepage displays

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>James Portfolio</h1>
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css'
+
+const Home = () => {
+    return (
+        <section className="homepageSection">
+            <div className="homepageContainer">
+
+            </div>
+        </section>
+    );
+}
+
+export default Home;
